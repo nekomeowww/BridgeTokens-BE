9a0a65d2d447ee838cc52685986d6457eea2f7b5
Ayaka Neko <neko@ayaka.moe>
2021-01-08 19:17:55 +0800

    :art: Applied routes for functions
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/contract.js
+++ src/api/contract.js
@@ -2,4 +2,9 @@ const KoaRouter = require('koa-router')
 
 const contractRouters = new KoaRouter()
 
+const { postRegistERC20TransferAction, postRegistMainAccountTransferAction } = require('../controller/registAction')
+
+contractRouters.post('/regist/transfer/ant', postRegistERC20TransferAction)
+contractRouters.post('/regist/transfer/account', postRegistMainAccountTransferAction)
+
 module.exports = contractRouters
