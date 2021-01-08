157d1c453cd49307c8c18532b9684883c5e2bed3
Ayaka Neko <neko@ayaka.moe>
2021-01-08 19:15:05 +0800

    :sparkles: Created ABI wrapper file for exporting
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

---
+++ src/constants/abi.js
@@ -0,0 +1,15 @@
+const ERC20ABI = require('./abi/ERC20.json')
+const ANT_ERC20 = require('./abi/ERC20Basic.json')
+
+const ANT = {
+    ANT_ERC20
+}
+
+const ERC20 = {
+    ERC20ABI
+}
+
+module.exports = {
+    ANT,
+    ERC20
+}
