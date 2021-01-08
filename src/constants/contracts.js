3684dd7cd97c9c9831e13e4ac2a28f16fdfa1a4b
Ayaka Neko <neko@ayaka.moe>
2021-01-08 19:15:32 +0800

    :sparkles: Changed the format of the contract to support multi-network
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/constants/contracts.js
+++ src/constants/contracts.js
@@ -1,4 +1,8 @@
-exports.rinkyby = {
-    ANT_ERC20: '0x85aa6FC792b1a9C4b6740bfA92B493C547B1EB5a',
+const rinkeby = {
+    ANT_ERC20: '0xec03Cb4B40BBdba56c1bD7C72bd85c35E9afbFB2',
     ANT_ERC20Multi: '0x192D7Df1473858a492487F666Ae6F32B4266f8Cc'
+}
+
+module.exports = {
+    rinkeby
 }
