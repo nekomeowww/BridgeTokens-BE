c479cb95af6dfafcc99608251ceb5b3403a8c674
Ayaka Neko <neko@ayaka.moe>
2021-01-08 19:16:19 +0800

    :sparkles: Added Global event listener
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

---
+++ src/event/listener.js
@@ -0,0 +1,8 @@
+const transferEvent = require('../event/transfer')
+
+const EventListener = () => {
+    // transferEvent.ERC20Transfer.listener()
+    // transferEvent.MainAccountTransfer.listener()
+}
+
+module.exports = EventListener
