c95a1b19d53b4f430aeeb070f1f75232bd58198c
Ayaka Neko <neko@ayaka.moe>
2021-01-12 15:42:27 +0800

    :sparkles: Added heco and binance contract
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/constants/contracts.js
+++ src/constants/contracts.js
@@ -1,8 +1,18 @@
 const rinkeby = {
-    ANT_ERC20: '0xec03Cb4B40BBdba56c1bD7C72bd85c35E9afbFB2',
-    ANT_ERC20Multi: '0x192D7Df1473858a492487F666Ae6F32B4266f8Cc'
+    ANT: '0xec03Cb4B40BBdba56c1bD7C72bd85c35E9afbFB2',
+    ANTMulti: '0x192D7Df1473858a492487F666Ae6F32B4266f8Cc'
+}
+
+const binanceTest = {
+    ANT: '0xfE0b1b4bef7d1E43BE2B518e1A86dC07E17d997D',
+}
+
+const heco = {
+    ANT: '0x3d1a6da59d87e8fe650b959314fdec53e6da56f1',
 }
 
 module.exports = {
-    rinkeby
+    rinkeby,
+    binanceTest,
+    heco
 }
