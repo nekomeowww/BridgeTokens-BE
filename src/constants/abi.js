86a5d7fa6298b64b1bb8dedda28a6ec07b57e228
Ayaka Neko <neko@ayaka.moe>
2021-01-12 15:43:20 +0800

    :sparkles: Changed ABI set
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/constants/abi.js
+++ src/constants/abi.js
@@ -1,13 +1,9 @@
 const ERC20ABI = require('./abi/ERC20.json')
 const ANT_ERC20 = require('./abi/ERC20Basic.json')
 
-const ANT = {
-    ANT_ERC20
-}
+const ANT = ANT_ERC20
 
-const ERC20 = {
-    ERC20ABI
-}
+const ERC20 = ERC20ABI
 
 module.exports = {
     ANT,
