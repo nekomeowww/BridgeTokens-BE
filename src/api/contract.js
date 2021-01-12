444f970e762db80256a712fc084452e47061a904
Ayaka Neko <neko@ayaka.moe>
2021-01-12 21:04:51 +0800

    :sparkles: Added new endpoints
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/contract.js
+++ src/api/contract.js
@@ -6,6 +6,7 @@ const contractRouters = new KoaRouter()
 
 const { postRegistERC20TransferAction, postRegistMainAccountTransferAction } = require('../controller/registAction')
 const { postConfirmERC20TransferAction, postConfirmMainAccountTransferAction } = require('../controller/confirmBridgeAction')
+const { getStatusMainAccountTransferAction } = require('../controller/statusEvent')
 
 contractRouters.use(async (ctx, next) => {
     const stream = new SSEStream()
@@ -17,5 +18,6 @@ contractRouters.post('/regist/transfer/erc20', postRegistERC20TransferAction)
 contractRouters.post('/regist/transfer/account', postRegistMainAccountTransferAction)
 contractRouters.post('/confirm/transfer/erc20', postConfirmERC20TransferAction)
 contractRouters.post('/confirm/transfer/account', postConfirmMainAccountTransferAction)
+contractRouters.get('/status', getStatusMainAccountTransferAction)
 
 module.exports = contractRouters
