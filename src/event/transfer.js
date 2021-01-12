f7b02428ecfe94b389eaefc6a26cfa547e8d78b1
Ayaka Neko <neko@ayaka.moe>
2021-01-12 21:07:31 +0800

    :sparkles: Added block scanner
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/event/transfer.js
+++ src/event/transfer.js
@@ -3,7 +3,6 @@ const web3 = require('../module/web3')
 const Web3Event = require('../event/eventEmitter')
 const contracts = require('../constants/contracts')
 const ABI = require('../constants/abi')
-const { MainAccount } = require('../../config.json')
 
 /**
  * ERC20Transfer 类型作用是监听 ERC20 代币的地址，并监听全局的 transfer 事件，同时
@@ -58,31 +57,58 @@ const MainAccountTransfer = {
     eventEmitter: new Web3Event(),
     /**
      * 该方法提供一个可调用接口供其他调度模块和服务模块调用
-     * 传入三个参数，该方法将从传入的三个方法中判断是否使用传入参数的合约地址，亦或是预设值
+     * 传入三个参数，该方法将从传入的三个参数中判断是否使用传入参数的合约地址，亦或是预设值
+     * @param {String} account              - 转账的目标用户
      * @param {String} network              - 区块链网络名称，全小写
      * @param {String} contractName         - 合约名称，默认值为 ANT，如果该值为 CUSTOM 则载入传入的合约地址
      * @param {String} contractAddr         - 合约地址，只有当 contractName 为 CUSTOM 时才会生效
      */
-    listener: async function (network, contractName, contractAddr) {
-        let contract
-        if (contractName === 'CUSTOM') {
-            contract = new web3[network].eth.Contract(ABI['ERC20'], contractAddr)
-        }
-        else contract = new web3[network].eth.Contract(ABI[contractName], contracts[network][contractName])
-        contract.events.Transfer()
-        .on('data', (event) => {
-            if (event.returnValues.to === MainAccount) this.eventEmitter.emit('transfer', undefined, event)
-        })
-        .on('error', (error) => {
-            this.eventEmitter.emit('transfer', error, undefined)
-        })
+    listener: async function (account, network) {
+        const id = setInterval(async () => {
+            let block = await web3[network].eth.getBlock('latest')
+            console.log(`[*] Searching block ${block.number}...`)
+            if (block && block.transactions) {
+                for (let txHash of block.transactions) {
+                    let tx
+                    try { tx = await web3[network].eth.getTransaction(txHash) }
+                    catch { continue }
+
+                    let txText
+                    try { txText = JSON.stringify(tx) }
+                    catch { continue }
+
+                    if (account === tx.to) {
+                        console.log(`[+] Transaction found on block ${block.number}`)
+                        res = tx
+                        this.eventEmitter.emit('transfer', undefined, res)
+                    }
+                }
+            }
+        }, 2000)
+
+        this.intervalId = id
     },
     delete: function () {
         delete this.listener
-    }
+        clearInterval(this.intervalId)
+    },
+    intervalId: null
 }
 
 module.exports = {
     ERC20Transfer,
     MainAccountTransfer
 }
+
+// let contract
+// if (contractName === 'CUSTOM') {
+//     contract = new web3[network].eth.Contract(ABI['ERC20'], contractAddr)
+// }
+// else contract = new web3[network].eth.Contract(ABI[contractName], contracts[network][contractName])
+// contract.events.Transfer()
+// .on('data', (event) => {
+//     if (event.returnValues.to === MainAccount) this.eventEmitter.emit('transfer', undefined, event)
+// })
+// .on('error', (error) => {
+//     this.eventEmitter.emit('transfer', error, undefined)
+// })
